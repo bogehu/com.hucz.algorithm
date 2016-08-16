1ad34cb6d67a28099f07024c864b07326d76114d
bogehu <bogehu@hucongzheng.cn>
2016-08-17 00:02:35 +0800

    Modifying the bubble sorted method to reduce the traversal times.

---
+++ src/main/java/sort/ModifiedBubbleSort.java
@@ -0,0 +1,21 @@
+package sort;
+
+/**
+ * Created by bogehu on 16/8/16.
+ */
+public class ModifiedBubbleSort {
+    public static void bubbleSort(int[] list){
+        boolean needNextPass=true;
+        for (int k = 0; k <list.length&&needNextPass ; k++) {
+           needNextPass=false;
+            for (int i = 0; i <list.length-k ; i++) {
+               if (list[i]>list[i+1]){
+                   int temp = list[i];
+                   list[i] = list[i+1];
+                   list[i+1] = temp;
+                   needNextPass=true;
+               }
+            }
+        }
+    }
+}
