4a4bd9a3d216eadb05fac96fcd0210c323fbe7f9
bogehu <bogehu@hucongzheng.cn>
2016-08-20 00:02:44 +0800

    Use  arrayList to realize heap and heapsort.

---
+++ src/main/java/sort/heapsort/Heap.java
@@ -0,0 +1,70 @@
+package sort.heapsort;
+
+/**
+ * Created by bogehu on 16/8/17.
+ */
+class Heap<E extends Comparable> {
+    //这里选择用arrayList来实现堆
+    private java.util.ArrayList<E> list = new java.util.ArrayList<E>();
+    //这里注意,看完不能想当然将构造方法写成public heap<E>(){}
+    public Heap() {
+    }
+
+
+    public Heap(E[] objects) {
+        for (int i = 0; i < objects.length; i++) {
+            add(objects[i]);
+        }
+    }
+
+    public void add(E newObject) {
+        list.add(newObject);
+        int currentIndex = list.size() - 1;
+        while (currentIndex > 0) {
+            int parentIndex = (currentIndex - 1) / 2;
+            if (list.get(currentIndex).compareTo(list.get(parentIndex)) > 0) {
+                E temp = list.get(currentIndex);
+                list.set(currentIndex, list.get(parentIndex));
+                list.set(parentIndex, temp);
+            } else
+                break;
+            currentIndex = parentIndex;
+        }
+
+    }
+
+    public E remove() {
+        if (list.size() == 0) return null;
+
+        E removedObject = list.get(0);
+        list.set(0, list.get(list.size() - 1));
+        list.remove(list.size() - 1);
+
+        int currentIndex = 0;
+        while (currentIndex < list.size()) {
+            int leftChildIndex = 2 * currentIndex + 1;
+            int rightChildIndex = 2 * currentIndex + 2;
+
+            if (leftChildIndex >= list.size()) break;
+            int maxIndex = leftChildIndex;
+            if (rightChildIndex < list.size()) {
+                if (list.get(maxIndex).compareTo(list.get(rightChildIndex)) < 0) {
+                    maxIndex = rightChildIndex;
+                }
+            }
+
+            if (list.get(currentIndex).compareTo(list.get(maxIndex)) < 0) {
+                E temp = list.get(maxIndex);
+                list.set(maxIndex, list.get(currentIndex));
+                list.set(currentIndex,temp);
+                currentIndex = maxIndex;
+            } else break;
+        }
+        return removedObject;
+
+    }
+
+    public int getSize() {
+        return list.size();
+    }
+}
